3a6730ed89d5ad3773bccefcc00c42f83709bad8
Kartik Luke Singh <kartikluke@gmail.com>
2013-11-29 01:08:52 +0530

    Fixed the wrong details in mailer

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,5 +1,5 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.sendgrid.net",
+  :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "sizrmailer@gmail.com",
